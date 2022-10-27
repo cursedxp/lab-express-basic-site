2463b374c06c3333134288b0d7ab3d0a4db799f3
cursedxp <anilozsoy@gmail.com>
2022-10-27 21:27:28 +0200

    home root/page added

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
 app.listen(port, () => {
   console.log("App listening on port 300");
 });
