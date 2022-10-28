ec9c30615f0037827435ce55281009b63dede82d
cursedxp <anilozsoy@gmail.com>
2022-10-28 13:08:08 +0200

    Home and about pages are added

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
 });
 
 app.listen(port, () => {
